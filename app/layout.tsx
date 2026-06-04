0ffbdc6ccaa57359912822aa8ba9350a1db8de37
Fred <fredkwesiga@MacBook-Pro.local>
2026-06-04 13:34:00 +0300

    works on the removing unwanted components

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,12 @@ export const metadata: Metadata = {
         url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
+      // {
+      //   url: '/icon.svg',
+      //   type: 'image/svg+xml',
+      // },
     ],
-    apple: '/apple-icon.png',
+    // apple: '/apple-icon.png',
   },
 }
 
